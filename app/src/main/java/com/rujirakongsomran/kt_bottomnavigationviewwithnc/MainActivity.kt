d20fe91645c24e7595ba66053b91c073ceed40cf
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-07-15 16:27:01 +0700

    Finish

--- app/src/main/java/com/rujirakongsomran/kt_bottomnavigationviewwithnc/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_bottomnavigationviewwithnc/MainActivity.kt
@@ -2,10 +2,31 @@ package com.rujirakongsomran.kt_bottomnavigationviewwithnc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.rujirakongsomran.kt_bottomnavigationviewwithnc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val navController = findNavController(R.id.fragmentContainerView)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.firstFragment,
+                R.id.secondFragment,
+                R.id.thirdFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
